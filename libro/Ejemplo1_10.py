c5c720a8a4fd358f87b15151a837045fccb78f06
alexsunseg <alejandrodelao411@gmail.com>
2019-09-12 22:16:54 -0500

    Problemas 1.10 y 1.11, funcionando

---
+++ libro/Ejemplo1_10.py
@@ -0,0 +1,7 @@
+BASE= int(input("Dame la base del rectangulo:"))
+ALTU= int(input("Dame la altura del rectangulo:"))
+SUP= BASE * ALTU
+PER= 2 * (BASE + ALTU)
+print ("El perimetro del rectangulo es de:", PER)
+print ("La superficie del rectangulo es de:", SUP)
+
